0a89051e7ebe3ea73b613b91c1469ad5e969fe2e
nilaybhatia <bhatianilay@gmail.com>
2019-08-15 11:03:06 +0530

    Progress with modifying date in JSON

--- launches/views.py
+++ launches/views.py
@@ -4,7 +4,6 @@ import requests, datetime
 # Create your views here
 def returnResponse(request):
 	queries = {'filter': 'flight_number,launch_date_utc,rocket/rocket_name,links/mission_patch'}
-	r = requests.get('https://api.spacexdata.com/v3/launches', params = queries)
-	#print(r.url)
-	return HttpResponse(r.json())
+	response = requests.get('https://api.spacexdata.com/v3/launches', params = queries)
+	return HttpResponse(response.json())
 #flight_number, launch_date_utc, rocket_name, mission_patch
