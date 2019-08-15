93c75c0686ff42303760423479cb9f11bdc5a744
nilaybhatia <bhatianilay@gmail.com>
2019-08-15 23:03:26 +0530

    Task 2 done

--- launches/views.py
+++ launches/views.py
@@ -4,6 +4,23 @@ import requests, datetime
 # Create your views here
 def returnResponse(request):
 	queries = {'filter': 'flight_number,launch_date_utc,rocket/rocket_name,links/mission_patch'}
+	#flight_number, launch_date_utc, rocket_name, mission_patch
+
 	response = requests.get('https://api.spacexdata.com/v3/launches', params = queries)
-	return HttpResponse(response.json())
-#flight_number, launch_date_utc, rocket_name, mission_patch
+	copy = response.json()
+
+	for data in copy:
+		launch_date_pretty = datetime.datetime.strptime(data['launch_date_utc'], "%Y-%m-%dT%H:%M:%S.%fZ").date()
+		#strptime parses a string representing a time according to a format
+
+		data['launch_date_utc'] = launch_date_pretty.strftime('%d %b, %Y')
+		#strftime formats a tuple representing a date/time to a string
+		
+		#Now we've to remove the nesting present in rocket and links keys
+		data['rocket-name'] = data['rocket']['rocket_name']
+		del data['rocket']
+		data['mission_patch'] = data['links']['mission_patch']
+		del data['links']
+
+	return HttpResponse(copy)
+
