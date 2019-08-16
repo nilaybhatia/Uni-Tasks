14541eadac03f4cffc479f52cc7307eb1c64b583
nilaybhatia <bhatianilay@gmail.com>
2019-08-16 12:53:21 +0530

    Created model for launch data

--- launches/models.py
+++ launches/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class LaunchData(models.Model):
+    flight_number = models.PositiveSmallIntegerField()
+    launch_date = models.DateTimeField()
+    rocket_name = models.TextField()
+    mission_patch_link = models.URLField()
