b34fb292d182df8e563c5420698e48d1a40984e3
nilaybhatia <bhatianilay@gmail.com>
2019-08-16 16:17:44 +0530

    Task 4 done

--- launches/models.py
+++ launches/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class LaunchData(models.Model):
     flight_number = models.PositiveSmallIntegerField()
-    launch_date = models.DateTimeField()
+    launch_date = models.DateField()
     rocket_name = models.TextField()
     mission_patch_link = models.URLField()
+
+    def __str__(self):
+        return self.flight_number
--- launches/views.py
+++ launches/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import LaunchData
 from django.template.loader import render_to_string
 import requests, datetime
 # Create your views here
@@ -9,20 +10,17 @@ def returnResponse(request):
 
 	response = requests.get('https://api.spacexdata.com/v3/launches', params = queries)
 	copy = response.json()
-
 	for data in copy:
 		launch_date_pretty = datetime.datetime.strptime(data['launch_date_utc'], "%Y-%m-%dT%H:%M:%S.%fZ").date()
-		#strptime parses a string representing a time according to a format
+		LaunchData.objects.create(
+			flight_number = data['flight_number'], 
+			launch_date = launch_date_pretty, 
+			rocket_name = data['rocket']['rocket_name'], 
+			mission_patch_link = data['links']['mission_patch'] if (data['links']['mission_patch'] is not None) else 'https://spaceflightnow.com/launch-schedule/')
 
-		data['launch_date_utc'] = launch_date_pretty.strftime('%d %b, %Y')
-		#strftime formats a tuple representing a date/time to a string
-		
-		#Now we've to remove the nesting present in rocket and links keys
-		data['rocket_name'] = data['rocket']['rocket_name']
-		del data['rocket']
-		data['mission_patch'] = data['links']['mission_patch']
-		del data['links']
-	rendered = render_to_string('launches/launches_list.html', {'data': copy})
+	launch_data =  	LaunchData.objects.all()
+	rendered = render_to_string('launches/launches_list.html', {'data': launch_data})
 	return HttpResponse(rendered)
 	#Or we can simply use the render function. But the task explicitly mentions HttpResponse object 
 	#return render(request, 'launches/launches_list.html', {'data' : copy})
+	
